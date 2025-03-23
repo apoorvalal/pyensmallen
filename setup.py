4445066e45cce618255c4f190c25e87f4785d9a6
Apoorva Lal <lal.apoorva@gmail.com>
2025-03-23 15:56:21 -0700

    Bump version to 0.2.1 to resolve PyPI filename conflict

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import Extension, find_packages, setup
 from setuptools.command.build_ext import build_ext
 from setuptools.dist import Distribution
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 
 class BinaryDistribution(Distribution):
