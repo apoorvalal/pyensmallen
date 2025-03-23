5c7bc3ac5f21bc1d27ff7b2fb79b57af007e1720
Apoorva Lal <lal.apoorva@gmail.com>
2025-03-23 16:13:38 -0700

    Fix Ensmallen header discovery during wheel building

--- setup.py
+++ setup.py
@@ -1,10 +1,47 @@
+import os
 import sys
+import platform
 
 import pybind11
 import setuptools
 from setuptools import Extension, find_packages, setup
 from setuptools.command.build_ext import build_ext
 from setuptools.dist import Distribution
+
+# Define possible ensmallen include locations
+def get_ensmallen_include():
+    """Get possible include paths for ensmallen library."""
+    # Check environment variables first
+    if 'ENSMALLEN_INCLUDE' in os.environ:
+        return [os.environ['ENSMALLEN_INCLUDE']]
+    
+    # Common locations on various platforms
+    paths = []
+    
+    # Check micromamba/conda paths if using CI
+    if 'MAMBA_ROOT_PREFIX' in os.environ:
+        if platform.system() == 'Darwin':
+            paths.append(os.path.join(os.environ['MAMBA_ROOT_PREFIX'], 'envs', 'pyensmallen', 'include'))
+        else:
+            paths.append('/host' + os.path.join(os.environ['MAMBA_ROOT_PREFIX'], 'envs', 'pyensmallen', 'include'))
+    
+    # Add other common system locations
+    if platform.system() == 'Linux':
+        paths.extend([
+            '/usr/include',
+            '/usr/local/include',
+            '/opt/homebrew/include',
+        ])
+    elif platform.system() == 'Darwin':
+        paths.extend([
+            '/usr/local/include',
+            '/opt/homebrew/include',
+        ])
+    
+    # Add CI-specific paths from environment variables
+    if 'CMAKE_PREFIX_PATH' in os.environ:
+        cmake_prefix = os.environ['CMAKE_PREFIX_PATH']
+        paths.append(os.path.join(cmake_prefix, 'include'))
 
 
 class BinaryDistribution(Distribution):
@@ -24,17 +61,63 @@ class get_pybind_include(object):
         return pybind11.get_include(self.user)
 
 
+    # Return all the possible include paths
+    return paths
+
+def get_include_dirs():
+    """Get all include directories needed for compilation."""
+    include_dirs = [
+        get_pybind_include(),
+        get_pybind_include(user=True),
+    ]
+    
+    # Add ensmallen include locations
+    include_dirs.extend(get_ensmallen_include())
+    
+    # Print the include paths for debugging in CI
+    print("Include paths being searched:")
+    for path in include_dirs:
+        print(f"  - {path}")
+    
+    return include_dirs
+
+def get_library_dirs():
+    """Get library directories for linking."""
+    # Default library directories
+    library_dirs = []
+    
+    # Add platform specific defaults
+    if platform.system() == 'Linux':
+        library_dirs.append("/usr/lib/x86_64-linux-gnu/")
+    
+    # Add micromamba lib dirs if available
+    if 'MAMBA_ROOT_PREFIX' in os.environ:
+        if platform.system() == 'Darwin':
+            library_dirs.append(os.path.join(os.environ['MAMBA_ROOT_PREFIX'], 'envs', 'pyensmallen', 'lib'))
+        else:
+            library_dirs.append('/host' + os.path.join(os.environ['MAMBA_ROOT_PREFIX'], 'envs', 'pyensmallen', 'lib'))
+    
+    # Add paths from CMAKE_PREFIX_PATH if available
+    if 'CMAKE_PREFIX_PATH' in os.environ:
+        cmake_prefix = os.environ['CMAKE_PREFIX_PATH']
+        library_dirs.append(os.path.join(cmake_prefix, 'lib'))
+    
+    # Print the library paths for debugging
+    print("Library paths being searched:")
+    for path in library_dirs:
+        print(f"  - {path}")
+    
+    return library_dirs
+
 ext_modules = [
     Extension(
         "pyensmallen._pyensmallen",
         ["pyensmallen/module.cpp"],
-        include_dirs=[
-            get_pybind_include(),
-            get_pybind_include(user=True),
-        ],
-        libraries=["armadillo", "lapack", "blas"],  # Add LAPACK and BLAS
-        library_dirs=["/usr/lib/x86_64-linux-gnu/"],  # Add LAPACK and BLAS
+        include_dirs=get_include_dirs(),
+        libraries=["armadillo", "ensmallen", "lapack", "blas"],
+        library_dirs=get_library_dirs(),
         language="c++",
+        define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')],
     ),
 ]
 
